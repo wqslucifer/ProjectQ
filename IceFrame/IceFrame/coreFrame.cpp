a92ed3f4928968fb2dc02be894de50c152a3b80b
QS <qslucifer@gmail.com>
2018-12-23 23:32:45 -0800

    loadCSV Function
    move loadCSV function from old project
    need to be tested

--- IceFrame/IceFrame/coreFrame.cpp
+++ IceFrame/IceFrame/coreFrame.cpp
@@ -1,11 +1,74 @@
 #include "coreFrame.h"
 
-#include <iostream>
-using namespace std;
-
 void IceFrame::loadCSV(string filename)
 {
+	fstream file;
+	try
+	{
+		file.open(filename.c_str(), std::ios::in);
+		if (!file.is_open()) {
+			cout << "file not open" << endl;
+			return;
+		}
+		rows = 0;
+		cols = 0;
+		// get csv file column names
+		string tempLine;
+		if (getline(file, tempLine)) {
+			istringstream sin(tempLine);
+			string colName;
+			int colIndex = 0;
+			while (getline(sin, colName, ','))
+			{
+				columns.push_back(colName);
+				colName_Str_Int.insert(pair<string, int>(colName, colIndex));
+				++cols;
+			}
+		}
+		else {
+			throw "CSV_READ_ERROR";
+		}
+		this->colTypes.assign(columns.size(), dtype_empty);
 
+		// load data
+		string cellBuff;
+		string tempLine;
+		while (getline(file, tempLine)) {
+			istringstream sin(tempLine);
+			vector<VAR> oneRow;
+			int col = 0;
+			while (getline(sin, cellBuff, delim))
+			{
+				VAR cell;
+				if (regex_match(cellBuff, sm, numeric_int) && dtype_int >= colTypes[col]) {
+					colTypes[col] = dtype_int;
+					cell = stoi(cellBuff);
+				}
+				else if (regex_match(cellBuff, sm, numeric) && dtype_double >= colTypes[col]) {
+					colTypes[col] = dtype_double;
+					cell = stod(cellBuff);
+				}
+				else if (dtype_string >= colTypes[col]) {
+					colTypes[col] = dtype_string;
+					cell = cellBuff;
+				}
+				else {
+					cell = string("NAN");
+				}
+				oneRow.push_back(cell);
+				++col;
+			}
+			for (auto c = col; c < columns.size(); c++)
+				oneRow.push_back(string(""));
+			iceData.push_back(oneRow);
+			++rows;
+		}
+		isEmpty = cols + rows > 0 ? false : true;
+	}
+	catch (const std::exception& e)
+	{
+		cout << "unexpected error: " << e.what() << endl;
+	}
 }
 
 void IceFrame::getSize()
@@ -34,7 +97,22 @@ void forArgs(TF&& mFn, Ts&&... mXs)
 }
 
 
+struct TypeCheckVisitor {
+	int operator()(const int n) const {
+		return dtype_int;
+	}
+	int operator()(const double b) const {
+		return dtype_double;
+	}
+	int operator()(const string& s) const {
+		return dtype_string;
+	}
+};
+
+
 int main() {
-	forArgs([](auto a) {cout << a << endl; }, 3, 4.0, "5");
+	variant<int, double, string> var;
+	string temp = "1234563546351";
+	cout << std::stoi(temp) << endl;
 	system("pause");
 }
--- IceFrame/IceFrame/coreFrame.h
+++ IceFrame/IceFrame/coreFrame.h
@@ -1,13 +1,36 @@
 #pragma once
-
 #include <cstdio>
 #include <string>
 #include <vector>
 #include <map>
 #include <algorithm>
 #include <list>
+#include <variant>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/regex.hpp>
+
+#define UNKNOWN_TYPE -1
 
 using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::pair;
+using std::map;
+using std::stoi;
+using std::stod;
+using std::fstream;
+using std::variant;
+using std::istringstream;
+
+constexpr int dtype_empty = -1;
+constexpr int dtype_int = 0;
+constexpr int dtype_double = 1;
+constexpr int dtype_string = 2;
 
 /*
 Ice Frame:
@@ -19,9 +42,25 @@ public:
 	int rows;
 	int cols;
 	int *size;
+	char delim;
+	vector<string> columns;
+	vector<int> colTypes;
+	bool isEmpty;
+	boost::smatch sm;
+	boost::regex numeric;
+	boost::regex numeric_int;
+	IceFrame() {
+		delim = ',';
+		isEmpty = true;
+		numeric = boost::regex("^(\\-|\\+)?0|[1-9]\\d*+(\\.\\d+)?$");
+		numeric_int = boost::regex("^(\\-|\\+)?0|[1-9]\\d*$");
+	}
 private:
+	typedef variant<int, double, string> VAR;
+	vector<vector<VAR>> iceData;
+	map<string, int> colName_Str_Int;
+	//////////////////////////////////////////////////////////////////////////
 	void loadCSV(string filename);
 	void getSize();
 protected:
 };
-
