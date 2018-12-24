bc68e407a7bd215b097a6e095f18ebe71443b0a5
QS <qslucifer@gmail.com>
2018-12-23 16:28:29 -0800

    init iceFrame and ignore file

---
+++ IceFrame/IceFrame/coreFrame.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <list>
+
+/*
+Ice Frame:
+IceFrame is a data frame designed for machine learning and other data mining algorithm
+*/
+
+class IceFrame {
+public:
+
+private:
+protected:
+};
