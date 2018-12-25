f371a21dc1bd456ef8079f3ef5adf3a6e0093e91
QS <qslucifer@gmail.com>
2018-12-24 18:39:11 -0800

    custom csv load function

--- IceFrame/IceFrame/coreFrame.cpp
+++ IceFrame/IceFrame/coreFrame.cpp
@@ -1,5 +1,6 @@
 #include "coreFrame.h"
 
+
 void IceFrame::loadCSV(string filename)
 {
 	fstream file;
@@ -18,7 +19,7 @@ void IceFrame::loadCSV(string filename)
 			istringstream sin(tempLine);
 			string colName;
 			int colIndex = 0;
-			while (getline(sin, colName, ','))
+			while (getline(sin, colName, delim))
 			{
 				columns.push_back(colName);
 				colName_Str_Int.insert(pair<string, int>(colName, colIndex));
@@ -31,13 +32,11 @@ void IceFrame::loadCSV(string filename)
 		this->colTypes.assign(columns.size(), dtype_empty);
 
 		// load data
-		string cellBuff;
-		string tempLine;
 		while (getline(file, tempLine)) {
-			istringstream sin(tempLine);
 			vector<VAR> oneRow;
 			int col = 0;
-			while (getline(sin, cellBuff, delim))
+			string cellBuff;
+			while (getOneCell(tempLine, cellBuff, delim))
 			{
 				VAR cell;
 				if (regex_match(cellBuff, sm, numeric_int) && dtype_int >= colTypes[col]) {
@@ -48,7 +47,7 @@ void IceFrame::loadCSV(string filename)
 					colTypes[col] = dtype_double;
 					cell = stod(cellBuff);
 				}
-				else if (dtype_string >= colTypes[col]) {
+				else if (cellBuff!="" && dtype_string >= colTypes[col]) {
 					colTypes[col] = dtype_string;
 					cell = cellBuff;
 				}
@@ -71,12 +70,87 @@ void IceFrame::loadCSV(string filename)
 	}
 }
 
+
+bool IceFrame::getOneCell(string &oneLine, string &cell, char delim)
+{
+	int cellLen = 0;
+	bool ifBlock = false;
+	char cellEnd = delim;
+	cell.clear();
+	if (oneLine.size() == 0)
+		return false;
+	char c;
+	while (cellLen < oneLine.size())
+	{
+		c = oneLine[cellLen];
+		if (c == '\"') {
+			if (!ifBlock)
+				ifBlock = true;
+			if (oneLine[cellLen + 1] == ',') {
+				++cellLen;
+				ifBlock = false;
+				break;
+			}
+			else if (oneLine[cellLen + 1] == '\"') {
+				cell += '"';
+				cellLen += 2;
+			}
+			else {
+				++cellLen;
+			}
+		}
+		else if (c == ',' && !ifBlock) {
+			break;
+		}
+		else{
+			cell += c;
+			++cellLen;
+		}
+	}
+	if (cellLen < oneLine.size()) {
+		oneLine = oneLine.substr(cellLen + 1);
+	}
+	else {
+		oneLine = oneLine.substr(cellLen);
+	}
+	return true;
+
+}
+
 void IceFrame::getSize()
 {
 	this->size = new int[2];
 	this->size[0] = this->rows;
 	this->size[1] = this->cols;
 }
+
+void IceFrame::setDelim(char delim) { this->delim = delim; }
+
+void IceFrame::display()
+{
+	for (int row = 0; row < rows; row++)
+	{
+		for (int col = 0; col < cols; col++)
+		{
+			std::visit(coutVisitor(), this->iceData[row][col]);
+			cout << "\t";
+		}
+		cout << endl;
+	}
+}
+
+auto IceFrame::iloc(int row, int col)
+{
+	std::visit(coutVisitor(), iceData[row][col]);
+}
+
+#ifdef DEBUG
+void IceFrame::testRegex(string num)
+{
+	cout << num << " is int:" << regex_match(num, sm, numeric_int) << endl;
+	cout << num << " is number:" << regex_match(num, sm, numeric) << endl;
+}
+#endif // DEBUG
 
 template<typename... Args>
 bool all(Args... args) { return (... && args); }
@@ -110,9 +184,31 @@ struct TypeCheckVisitor {
 };
 
 
+void testfunc(variant<int, string> input) {
+	std::visit(coutVisitor(), input);
+}
+
 int main() {
-	variant<int, double, string> var;
-	string temp = "1234563546351";
-	cout << std::stoi(temp) << endl;
+	string csvPath = "E:/project/ProjectQ/test/TitanicDataset/train.csv";
+	IceFrame trainset;
+#ifdef DEBUGREGEX
+	trainset.testRegex("1000");
+	trainset.testRegex("0065");
+	trainset.testRegex("16354.5684");
+	trainset.testRegex("05");
+	trainset.testRegex("0");
+	trainset.testRegex("0.000");
+	trainset.testRegex("+.5615");
+	trainset.testRegex("+0.5615");
+	trainset.testRegex("-0.5615");
+
+	string temp = "\"adf,\"\"alllll\"\"\",3214,,13";
+	string cell;
+	while (trainset.getOneCell(temp, cell, ',')) {
+		cout << cell << endl;
+	}
+#endif // DEBUGREGEX
+	trainset.loadCSV(csvPath);
+	trainset.display();
 	system("pause");
 }
--- IceFrame/IceFrame/coreFrame.h
+++ IceFrame/IceFrame/coreFrame.h
@@ -9,23 +9,19 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
-#include <boost/regex.hpp>
+#include <regex>
 
 #define UNKNOWN_TYPE -1
 
-using std::string;
-using std::vector;
-using std::cout;
-using std::cin;
-using std::endl;
-using std::pair;
-using std::map;
-using std::stoi;
-using std::stod;
-using std::fstream;
-using std::variant;
-using std::istringstream;
+// #define DEBUGREGEX
+
+#ifdef DEBUGREGEX
+#ifndef DEBUG
+#define DEBUG
+#endif
+#endif // DEBUGREGEX
+
+using namespace std;
 
 constexpr int dtype_empty = -1;
 constexpr int dtype_int = 0;
@@ -37,30 +33,51 @@ Ice Frame:
 IceFrame is a data frame designed for machine learning and other data mining algorithm
 */
 
+struct coutVisitor {
+	void operator()(const int input) const {
+		cout << input;
+	}
+	void operator()(const double input) const {
+		cout << input;
+	}
+	void operator()(const string& input) const {
+		cout << input;
+	}
+};
+
 class IceFrame {
 public:
 	int rows;
 	int cols;
 	int *size;
-	char delim;
 	vector<string> columns;
 	vector<int> colTypes;
 	bool isEmpty;
-	boost::smatch sm;
-	boost::regex numeric;
-	boost::regex numeric_int;
+	smatch sm;
+	regex numeric;
+	regex numeric_int;
 	IceFrame() {
 		delim = ',';
 		isEmpty = true;
-		numeric = boost::regex("^(\\-|\\+)?0|[1-9]\\d*+(\\.\\d+)?$");
-		numeric_int = boost::regex("^(\\-|\\+)?0|[1-9]\\d*$");
+		numeric_int.assign("^(\\-|\\+)?0|[1-9]\\d*$", std::regex::ECMAScript);
+		numeric.assign("(^(\\-|\\+)?0|[1-9]\\d*)(\\.\\d+)?$", std::regex::ECMAScript);
 	}
+	void setDelim(char delim);
+	void loadCSV(string filename);
+	void display(void);
+	auto iloc(int row, int col);
+	void loc(variant<int,string> row, variant<int, string> col);
+	bool getOneCell(string &oneLine, string &cell, char delim);
+#ifdef DEBUG
+	void testRegex(string num);
+#endif // DEBUG
 private:
+	char delim;
 	typedef variant<int, double, string> VAR;
 	vector<vector<VAR>> iceData;
 	map<string, int> colName_Str_Int;
 	//////////////////////////////////////////////////////////////////////////
-	void loadCSV(string filename);
 	void getSize();
+	//bool getOneCell(string &oneLine, string &cell, char delim);
 protected:
 };
